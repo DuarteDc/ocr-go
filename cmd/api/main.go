8e1942d7e2b91589d9d3dd089435f139387fd07e
DuarteDc <al221910409@gmail.com>
2026-08-12 00:01:40 -0600

    feat: implement server setup with health check endpoint

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,20 @@
 package api
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"github.com/DuarteDc/ocr-golang.git/internal/infraestructure/http"
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Print("Gola")
+	_ = godotenv.Load()
+	router := http.NewRouter()
+
+	log.Println("Server started on port 8080")
+
+	if err := router.Run(fmt.Sprintf(":%s", "8080")); err != nil {
+		log.Fatalf("Failed to start server: %v", err)
+	}
 }
